b78492d4c69dd6d4bd297bc4601f64186d851553
jonasvanloey <jonas.vanloey@student.kdg.be>
2017-11-09 19:37:55 +0100

    added bootstrap did some styling and ingr list

--- src/app/gerecht/gerecht.service.ts
+++ src/app/gerecht/gerecht.service.ts
@@ -4,5 +4,14 @@ import { Injectable } from '@angular/core';
 export class GerechtService {
 
   constructor() { }
+//JONAS
+//   getrecipe(){
+//     unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/479101/information?includeNutrition=false")
+//       .header("X-Mashape-Key", "JlriWXppBkmshvo2hWZ5wnSJLhSUp1Z1xNEjsnBi1CiXFKv2FE")
+//       .header("X-Mashape-Host", "spoonacular-recipe-food-nutrition-v1.p.mashape.com")
+//       .end(function (result) {
+//         console.log(result.status, result.headers, result.body);
+//       });
+//   }
 
 }
